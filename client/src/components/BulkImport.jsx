8da609f4fea57c5ba87b7b0ee7c58d3490987f67
Max Smith <ms724@duke.edu>
2021-02-07 18:07:41 -0500

    Validating models based on string length

--- client/src/components/BulkImport.jsx
+++ client/src/components/BulkImport.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import XLSX from 'xlsx';
-import CSVFileValidator from 'csv-file-validator';
+// import CSVFileValidator from 'csv-file-validator';
+const CSVFileValidator = require('csv-file-validator');
 
 export default function BulkImport() {
+  const characterLimits = {
+    model: {
+      vendor: 30,
+      modelNumber: 40,
+      shortDescription: 100,
+      comment: 200,
+      calibrationFrequency: 10,
+    },
+  };
+
   // const fileData = {
   //   models: [],
   //   instruments: [],
@@ -20,7 +31,16 @@ export default function BulkImport() {
   };
 
   // TODO - refactor into pretty UI error
-  const requiredError = (headerName, rowNumber, columnNumber) => `<div class="red">${headerName} is required in the <strong>${rowNumber} row</strong> / <strong>${columnNumber} column</strong></div>`;
+  const requiredError = (headerName, rowNumber, columnNumber) => {
+    // const message = `<div class="red">${headerName} is required in the <strong>${rowNumber} row</strong> / <strong>${columnNumber} column</strong></div>`;
+    const message = `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`;
+    return message;
+  };
+
+  const validateASCII = (field, length) => {
+    const validLength = field.length <= length;
+    return validLength;
+  };
 
   const modelConfig = {
     headers: [
@@ -29,29 +49,53 @@ export default function BulkImport() {
         inputName: 'vendor',
         required: true,
         requiredError,
+        validate(vendor) {
+          return validateASCII(vendor, characterLimits.model.vendor);
+        },
       },
       {
         name: 'Model Number',
         inputName: 'modelNumber',
         required: true,
         requiredError,
+        validate(modelNumber) {
+          return validateASCII(modelNumber, characterLimits.model.modelNumber);
+        },
       },
       {
         name: 'Short Description',
         inputName: 'shortDescription',
         required: true,
         requiredError,
+        validate(shortDescription) {
+          return validateASCII(shortDescription, characterLimits.model.shortDescription);
+        },
+      },
+      {
+        name: 'Comment',
+        inputName: 'comment',
+        required: true,
+        requiredError,
+
+        validate(comment) {
+          return validateASCII(comment, characterLimits.model.comment);
+        },
       },
       {
         name: 'Calibration Frequency',
         inputName: 'calibrationFrequency',
         required: true,
         requiredError,
+        validate(calibrationFrequency) {
+          return validateASCII(calibrationFrequency, characterLimits.model.calibrationFrequency);
+        },
       },
     ],
   };
 
   const parseModels = (worksheet) => {
+    console.log('Parsing models worksheet');
+
     console.log(XLSX.utils.sheet_to_row_object_array(worksheet));
     const modelCSV = XLSX.utils.sheet_to_csv(worksheet);
 
@@ -65,13 +109,13 @@ export default function BulkImport() {
       });
   };
 
-  const parseInstruments = (worksheet) => {
-    console.log(XLSX.utils.sheet_to_row_object_array(worksheet));
-  };
+  // const parseInstruments = (worksheet) => {
+  //   console.log(XLSX.utils.sheet_to_row_object_array(worksheet));
+  // };
 
-  const parseCalibration = (worksheet) => {
-    console.log(XLSX.utils.sheet_to_row_object_array(worksheet));
-  };
+  // const parseCalibration = (worksheet) => {
+  //   console.log(XLSX.utils.sheet_to_row_object_array(worksheet));
+  // };
 
   // Call a function (passed as a prop from the parent component)
   // to handle the user-selected file
@@ -93,8 +137,8 @@ export default function BulkImport() {
 
       // Parse each worksheet by its sheet names
       parseModels(readData.Sheets.Models);
-      parseInstruments(readData.Sheets.Instruments);
-      parseCalibration(readData.Sheets.Calibration);
+      // parseInstruments(readData.Sheets.Instruments);
+      // parseCalibration(readData.Sheets.Calibration);
     };
     reader.readAsBinaryString(f);
   };
