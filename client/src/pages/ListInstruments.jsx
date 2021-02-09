d574c9ef4e66b6a3c1d4d56cf7f89cb7d0a2f74a
Angel Huizar <ah426@duke.edu>
2021-02-08 18:56:17 -0800

    can now view most recent calibration date on instrument table; next step is to display if item is in calibration, out of calibration, non-calibratable, or within 30 days of expiring;

--- client/src/pages/ListInstruments.jsx
+++ client/src/pages/ListInstruments.jsx
@@ -1,11 +1,10 @@
+/* eslint-disable max-len */
 import { useState, useContext } from 'react';
-import { gql } from '@apollo/client';
-import { print } from 'graphql';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import SearchIcon from '@material-ui/icons/Search';
-import Query from '../components/UseQuery';
+import GetAllInstruments from '../queries/GetAllInstruments';
 import DisplayGrid from '../components/UITable';
 import MouseOverPopover from '../components/PopOver';
 import ModalAlert from '../components/ModalAlert';
@@ -30,28 +29,36 @@ function ListInstruments() {
   const [serialNumber, setSerialNumber] = useState('');
   // eslint-disable-next-line no-unused-vars
   const [id, setId] = useState('');
-  const GET_INSTRUMENTS_QUERY = gql`
-    query Instruments {
-      getAllInstruments {
-        id
-        vendor
-        modelNumber
-        serialNumber
-        description
-        calibrationFrequency
-      }
-    }
-  `;
-  const query = print(GET_INSTRUMENTS_QUERY);
-  const queryName = 'getAllInstruments';
   const handleResponse = (response) => {
     // console.log(`getAllInstruments\n${response}`);
-    // console.log(response);
+    response.forEach((element) => {
+      GetCalibHistory({
+        id: element.id,
+        mostRecent: true,
+        dateOnly: true,
+      }).then((value) => {
+        // eslint-disable-next-line no-param-reassign
+        element.date = (value) ? value.date : 'No history found';
+      });
+    });
     setInstruments(response);
   };
   if (!queried) {
     setQueried(true);
-    Query({ query, queryName, handleResponse });
+    GetAllInstruments({ handleResponse });
+    // const newCopy = rows;
+    // // console.log(response);
+    // rows.forEach((element) => {
+    //   GetCalibHistory({
+    //     id: element.id,
+    //     mostRecent: true,
+    //     dateOnly: true,
+    //   }).then((value) => {
+    //     const compositeData = { ...element, ...value }; // combine data objects
+    //     newCopy.push(compositeData); // put into new array
+    //   });
+    // });
+    // console.log(newCopy);
   }
   const cellHandler = (e) => {
     if (e.field === 'view' || e.field === 'delete' || e.field === 'edit') {
@@ -61,7 +68,7 @@ function ListInstruments() {
       setId(e.row.id);
       setSerialNumber(e.row.serialNumber);
       setShow(true);
-      GetCalibHistory({ id: e.row.id });
+      // GetCalibHistory({ id: e.row.id });
     }
   };
   const closeModal = (bool) => {
@@ -69,7 +76,7 @@ function ListInstruments() {
     setWhich('');
     if (bool) {
       // If updated successfully, update rows
-      Query({ query, queryName, handleResponse });
+      GetAllInstruments({ handleResponse });
     }
   };
   const handleRes = (response) => {
@@ -91,39 +98,9 @@ function ListInstruments() {
     },
     { field: 'vendor', headerName: 'Vendor', width: 150 },
     { field: 'modelNumber', headerName: 'Model Number', width: 150 },
-    { field: 'description', headerName: 'Description', width: 250 },
+    { field: 'description', headerName: 'Description', width: 225 },
     { field: 'serialNumber', headerName: 'Serial Number', width: 150 },
-    {
-      field: 'calibrationFrequency',
-      headerName: 'Calibration Frequency',
-      width: 200,
-      type: 'number',
-      renderCell: (params) => (
-        <div className="row">
-          <div className="col mt-3">
-            {params.value === 0 ? (
-              <MouseOverPopover message="Instrument not calibratable">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="48"
-                  height="48"
-                  fill="#f78102"
-                  className="bi bi-calendar-x"
-                  viewBox="0 0 32 32"
-                >
-                  {/* eslint-disable-next-line max-len */}
-                  <path d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z" />
-                  {/* eslint-disable-next-line max-len */}
-                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                </svg>
-              </MouseOverPopover>
-            ) : (
-              params.value
-            )}
-          </div>
-        </div>
-      ),
-    },
+    { field: 'date', headerName: 'Most Recent Calibration Date', width: 250 },
     {
       field: 'view',
       headerName: ' ',
@@ -226,7 +203,8 @@ function ListInstruments() {
           </div>
         )}
       </ModalAlert>
-      {DisplayGrid({ rows, cols, cellHandler })}
+      {/* eslint-disable-next-line object-shorthand */}
+      {DisplayGrid({ rows: rows, cols, cellHandler })}
     </div>
   );
 }
@@ -248,4 +226,36 @@ export default ListInstruments;
             viewOnly
           />
         )}
+            {
+      field: 'calibrationFrequency',
+      headerName: 'Calibration Frequency',
+      width: 200,
+      type: 'number',
+      renderCell: (params) => (
+        <div className="row">
+          <div className="col mt-3">
+            {params.value === 0 ? (
+              <MouseOverPopover message="Instrument not calibratable">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="48"
+                  height="48"
+                  fill="#f78102"
+                  className="bi bi-calendar-x"
+                  viewBox="0 0 32 32"
+                >
+
+                  // eslint-disable-next-line max-len
+                  <path d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z" />
+
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                </svg>
+              </MouseOverPopover>
+            ) : (
+              params.value
+            )}
+          </div>
+        </div>
+      ),
+    },
 */
--- client/src/queries/GetCalibHistory.jsx
+++ client/src/queries/GetCalibHistory.jsx
@@ -4,14 +4,27 @@ import PropTypes from 'prop-types';
 import Query, { QueryAndThen } from '../components/UseQuery';
 
 export default async function GetCalibHistory({
-  id, handleResponse,
+  id, mostRecent, handleResponse, dateOnly,
 }) {
   GetCalibHistory.propTypes = {
-    id: PropTypes.number.isRequired,
-    handleResponse: PropTypes.func,
+    // mostRecent only works if you don't pass a handler
+    mostRecent: PropTypes.bool, // If true, only return the most recent calib event, if it exists
+    id: PropTypes.number.isRequired, // ID of instrument
+    handleResponse: PropTypes.func, // Handler
+    dateOnly: PropTypes.bool, // If you only want the date
   };
-  // console.log(events);
-  const GET_CALIB_HIST = gql`
+  GetCalibHistory.defaultProps = {
+    mostRecent: false,
+    dateOnly: false,
+  };
+  const GET_DATE_HIST = gql`
+    query GetCalibHist($id: Int!) {
+      getCalibrationEventsByReferenceId(calibrationHistoryIdReference: $id) {
+        date
+      }
+    }
+  `;
+  const GET_ALL_CALIB_HIST = gql`
     query GetCalibHist($id: Int!) {
       getCalibrationEventsByReferenceId(calibrationHistoryIdReference: $id) {
         date
@@ -20,17 +33,21 @@ export default async function GetCalibHistory({
       }
     }
   `;
-  const query = print(GET_CALIB_HIST);
+  const query = dateOnly ? print(GET_DATE_HIST) : print(GET_ALL_CALIB_HIST); // Pick which query to use
   const queryName = 'getCalibrationEventsByReferenceId';
   const getVariables = () => ({ id });
-  if (handleResponse) {
+  if (handleResponse) { // If you passed a handler, call it
     Query({
       query, queryName, getVariables, handleResponse,
     });
-  } else {
-    // eslint-disable-next-line no-return-await
+  } else { // Else, return response
     const response = await QueryAndThen({ query, queryName, getVariables });
-    response.sort((a, b) => new Date(b.date) - new Date(a.date)); // This will sort calib events by date (most recent to least recent)
+    if (response) { // If response isn't null
+      response.sort((a, b) => new Date(b.date) - new Date(a.date)); // This will sort calib events by date (most recent to least recent)
+      if (mostRecent) { // If you asked for most recent, return it
+        return response[0];
+      }
+    }
     // console.log(response);
     return response;
   }
