a44752085c52d6536da3a78f54cb93bd4df260e2
Angel Huizar <ah426@duke.edu>
2021-02-08 20:06:41 -0800

    instruments now display differently depending on if instrument in calibration, out of calibration, non-calibratable, or within 30 days of calibration. Branch complete

--- client/src/pages/ListInstruments.jsx
+++ client/src/pages/ListInstruments.jsx
@@ -13,12 +13,14 @@ import DeleteInstrument from '../queries/DeleteInstrument';
 import EditInstrument from '../components/EditInstrument';
 import GetCalibHistory from '../queries/GetCalibHistory';
 
+// eslint-disable-next-line no-extend-native
+Date.prototype.addDays = function (days) { // This allows you to add days to a date object and get a new date object
+  const date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
 function ListInstruments() {
-  // const user = useContext(UserContext);
-  // console.log(user);
-  // if (!user.isLoggedIn) {
-  //   return <ErrorPage message="You need to sign in to see this page!" />;
-  // }
   const user = useContext(UserContext);
   const [rows, setInstruments] = useState([]);
   const [queried, setQueried] = useState(false);
@@ -37,8 +39,22 @@ function ListInstruments() {
         mostRecent: true,
         dateOnly: true,
       }).then((value) => {
+        const today = new Date();
         // eslint-disable-next-line no-param-reassign
-        element.date = (value) ? value.date : 'No history found';
+        element.date = value ? value.date : 'No history found';
+        // eslint-disable-next-line no-param-reassign
+        element.calibrationStatus = 'NA';
+        if (value) {
+          const nextCalibDate = new Date(value.date).addDays(
+            // Calculate next calibration date
+            element.calibrationFrequency,
+          );
+          const daysInBtwn = Math.round(
+            (nextCalibDate.getTime() - today.getTime()) / (1000 * 3600 * 24),
+          );
+          // eslint-disable-next-line no-param-reassign
+          element.calibrationStatus = daysInBtwn;
+        }
       });
     });
     setInstruments(response);
@@ -46,19 +62,6 @@ function ListInstruments() {
   if (!queried) {
     setQueried(true);
     GetAllInstruments({ handleResponse });
-    // const newCopy = rows;
-    // // console.log(response);
-    // rows.forEach((element) => {
-    //   GetCalibHistory({
-    //     id: element.id,
-    //     mostRecent: true,
-    //     dateOnly: true,
-    //   }).then((value) => {
-    //     const compositeData = { ...element, ...value }; // combine data objects
-    //     newCopy.push(compositeData); // put into new array
-    //   });
-    // });
-    // console.log(newCopy);
   }
   const cellHandler = (e) => {
     if (e.field === 'view' || e.field === 'delete' || e.field === 'edit') {
@@ -68,8 +71,16 @@ function ListInstruments() {
       setId(e.row.id);
       setSerialNumber(e.row.serialNumber);
       setShow(true);
-      // GetCalibHistory({ id: e.row.id });
     }
+  };
+  const genClassName = (daysLeft) => {
+    if (daysLeft > 30) {
+      return 'text-success';
+    }
+    if (daysLeft > 0 && daysLeft <= 30) {
+      return 'text-warning';
+    }
+    return 'text-danger';
   };
   const closeModal = (bool) => {
     setShow(false);
@@ -100,7 +111,45 @@ function ListInstruments() {
     { field: 'modelNumber', headerName: 'Model Number', width: 150 },
     { field: 'description', headerName: 'Description', width: 225 },
     { field: 'serialNumber', headerName: 'Serial Number', width: 150 },
-    { field: 'date', headerName: 'Most Recent Calibration Date', width: 250 },
+    {
+      field: 'date',
+      headerName: 'Most Recent Calibration',
+      width: 250,
+      type: 'date',
+    },
+    {
+      field: 'calibrationStatus',
+      headerName: 'Calibration Expiration',
+      width: 200,
+      type: 'number',
+      renderCell: (params) => (
+        <div className="row">
+          <div className="col mt-3">
+            {params.value === 'NA' ? (
+              <MouseOverPopover message="Instrument not calibratable">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="48"
+                  height="48"
+                  fill="#f78102"
+                  className="bi bi-calendar-x"
+                  viewBox="0 0 32 32"
+                >
+                  <path d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z" />
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                </svg>
+              </MouseOverPopover>
+            ) : (
+              <MouseOverPopover
+                message={`${params.value} days left till next calibration`}
+              >
+                <span className={genClassName(params.value)}>{params.value}</span>
+              </MouseOverPopover>
+            )}
+          </div>
+        </div>
+      ),
+    },
     {
       field: 'view',
       headerName: ' ',
