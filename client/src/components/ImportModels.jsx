ccc9e921b2acb1fcc4d48826d0b3a246ae46f22a
Max Smith <ms724@duke.edu>
2021-02-10 16:25:16 -0500

    Catching all errors by row

--- client/src/components/ImportModels.jsx
+++ client/src/components/ImportModels.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import CSVReader from 'react-csv-reader';
 import { camelCase } from 'lodash';
-import { gql } from '@apollo/client';
-import { print } from 'graphql';
-import Query from './UseQuery';
 import ModalAlert from './ModalAlert';
+// import { gql } from '@apollo/client';
+// import { print } from 'graphql';
+// import Query from './UseQuery';
 
 export default function ImportModels() {
   // mutation {
@@ -21,49 +21,42 @@ export default function ImportModels() {
   };
 
   // TODO: Fix gql query
-  const IMPORT_MODELS = gql`
-    type ModelType {
-      vendor: String!
-      modelNumber: String!
-      shortDescription: String!
-      comment: String
-      calibrationFrequency: Int
-    }
-    input ModelInput {
-      vendor: String!
-      modelNumber: String!
-      shortDescription: String!
-      comment: String
-      calibrationFrequency: Int
-    }
-    mutation ImportModels(
-      $data: [ModelInput]!
-    ) {
-      bulkImportData(
-        models: [ModelInput]
-      )
-    }  
-  `;
+  // const IMPORT_MODELS = gql`
+  //   type ModelType {
+  //     vendor: String!
+  //     modelNumber: String!
+  //     shortDescription: String!
+  //     comment: String
+  //     calibrationFrequency: Int
+  //   }
+  //   input ModelInput {
+  //     vendor: String!
+  //     modelNumber: String!
+  //     shortDescription: String!
+  //     comment: String
+  //     calibrationFrequency: Int
+  //   }
+  //   mutation ImportModels(
+  //     $data: [ModelInput]!
+  //   ) {
+  //     bulkImportData(
+  //       models: [ModelInput]
+  //     )
+  //   }
+  // `;
 
   // FIXME: Might need to add instruments to bulk import query
-
-  // const IMPORT_MODELS = gql`
-  //     mutation ImportModels(
-  //         $vendor: String!
-  //         $modelNumber: String!
-  //         $shortDescription: String!
-  //         $comment: String!
-  //         $calibrationFrequency: Int!
-  //       ) {
-  //         bulkImportData(
-  //           vendor: $vendor
-  //           modelNumber: $modelNumber
-  //           description: $shortDescription
-  //           comment: $comment
-  //           calibrationFrequency: $calibrationFrequency
-  //         )
-  //       }
-  //   `;
+  // var MovieSchema = `
+  // type Movie {
+  //  name: String
+  // }
+  // input MovieInput {
+  //  name: String
+  // }
+  // mutation {
+  //  addMovies(movies: [MovieInput]): [Movie]
+  // }
+  // `
 
   const characterLimits = {
     model: {
@@ -74,18 +67,6 @@ export default function ImportModels() {
       calibrationFrequency: 10,
     },
   };
-  //     instrument: {
-  //       vendor: 30,
-  //       modelNumber: 40,
-  //       serialNumber: 40,
-  //       comment: 200,
-  //       calibrationUsername: 50,
-  //       calibrationDate: 20,
-  //       calibrationComment: 200,
-  //     },
-  //   };
-
-  // Create a reference to the hidden file input element
 
   const papaparseOptions = {
     header: true,
@@ -94,126 +75,105 @@ export default function ImportModels() {
     transformHeader: (header) => camelCase(header),
   };
 
+  // NOTE: Headers have been modified by lodash to be camelCase for ease of import
   const requiredHeaders = ['vendor', 'modelNumber', 'shortDescription'];
 
-  const checkAllKeys = (data, keys) => keys.every((item) => Object.prototype.hasOwnProperty.call(data, item) && data[item]);
-
-  const validateKeys = (data) => {
-    // Check all headers in data:
+  // TODO: Refactor missingKey to match original import
+  const getMissingKeys = (row, keys) => {
     const missingKeys = [];
-    data.forEach((item, index) => {
-      console.log(item);
-      if (!checkAllKeys(item, requiredHeaders)) {
-        missingKeys.push(index);
+    keys.forEach((item) => {
+      if (!row[item]) {
+        missingKeys.push(item);
       }
     });
-    return missingKeys;
+    return missingKeys.length > 0 ? missingKeys : null;
   };
 
-  const checkDuplicateModels = (data) => {
-    const duplicateModels = [];
-    const modelMap = new Map();
-
-    // TODO: add index to forEach for better error handling
-    data.forEach((item) => {
-      const itemVendor = item.vendor;
-      const itemModelNumber = item.modelNumber;
-
-      // Check if map contains vendor
-      if (modelMap.has(itemVendor)) {
-        const vendorModels = modelMap.get(itemVendor);
-        if (vendorModels.indexOf(itemModelNumber) !== -1) {
-          duplicateModels.push({
-            vendor: itemVendor,
-            modelNumber: itemModelNumber,
-          });
-        }
-      } else {
-        modelMap.set(itemVendor, new Array(itemModelNumber));
+  const checkDuplicateModel = (data, vendor, modelNumber, myIndex) => {
+    let isDuplicateModel = false;
+    data.forEach((row, index) => {
+      if (index !== myIndex && row.vendor === vendor && row.modelNumber === modelNumber) {
+        isDuplicateModel = true;
       }
     });
-    return duplicateModels;
+    return isDuplicateModel;
   };
 
   const validateRow = (row) => {
     // TODO: Make this less ugly
     const invalidKeys = [];
-    if (row.vendor.length > characterLimits.model.vendor) {
+    if (row.vendor && row.vendor.length > characterLimits.model.vendor) {
       invalidKeys.push('Vendor');
     }
-    if (row.modelNumber.length > characterLimits.model.modelNumber) {
+    if (row.modelNumber && row.modelNumber.length > characterLimits.model.modelNumber) {
       invalidKeys.push('Model-Number');
     }
-    if (row.shortDescription.length > characterLimits.model.shortDescription) {
+    if (row.shortDescription && row.shortDescription.length > characterLimits.model.shortDescription) {
       invalidKeys.push('Short-Description');
     }
-    if (row.comment.length > characterLimits.model.comment) {
+    if (row.comment && row.comment.length > characterLimits.model.comment) {
       invalidKeys.push('Comment');
     }
-    if (row.calibrationFrequency.length > characterLimits.model.calibrationFrequency) {
+    if (row.calibrationFrequency && row.calibrationFrequency.length > characterLimits.model.calibrationFrequency) {
       invalidKeys.push('Calibration-Frequency');
     }
-    return invalidKeys;
+    return invalidKeys.length > 0 ? invalidKeys : null;
   };
 
-  const validateAllFields = (data) => {
-    const allInvalidFields = [];
-    data.forEach((item, index) => {
-      const invalidKeys = validateRow(item);
-      if (invalidKeys.length > 0) {
-        allInvalidFields.push({
-          row: index + 1,
-          invalidKeys,
-        });
-      }
-    });
-    return allInvalidFields;
-  };
+  const validateCalibrationFrequency = (calibrationFrequency) => calibrationFrequency >= 0 || calibrationFrequency === 'N/A';
 
   const handleCSVReader = (data /* , fileInfo */) => {
-    // TODO: Refactor, single loop over all data values, give errors row by row (instead of by concept)
-    // TODO: Handle (NA) in calibrationFrequency (filter this to null value?)
-    // Validate headers
-    const missingKeys = validateKeys(data);
-    if (missingKeys.length) {
-      console.log(`ERROR: Missing keys in rows: ${missingKeys}`);
-      setShow(true);
-      return;
-    }
+    const allRowErrors = [];
+    data.forEach((row, index) => {
+      // Check missing keys
+      const missingKeys = getMissingKeys(row, requiredHeaders);
 
-    // Check for duplicate vendor/model number in sheet
-    const duplicateModels = checkDuplicateModels(data);
-    if (duplicateModels.length) {
-      console.log('ERROR: Duplicate models: ');
-      console.log(duplicateModels);
-    }
+      let isDuplicateModel;
+      if (row.vendor && row.modelNumber) {
+        isDuplicateModel = checkDuplicateModel(data, row.vendor, row.modelNumber, index);
+      }
 
-    // Check character limits by field
-    // TODO: Specific character validation
-    const allInvalidFields = validateAllFields(data);
-    if (allInvalidFields.length) {
-      console.log('ERROR: Invalid field lengths: ');
-      console.log(allInvalidFields);
-    }
+      // Validate entries by length
+      const invalidEntries = validateRow(row);
 
-    // Now all fields have been validated, time to attempt a db push...
-    const query = print(IMPORT_MODELS);
-    const queryName = 'bulkImportData';
+      // Validate calibration frequency
+      const invalidCalibration = !validateCalibrationFrequency(row.calibrationFrequency);
 
-    const getVariables = () => ({
-      data,
+      // If any errors exist, create errors object
+      if (missingKeys || invalidEntries || invalidCalibration || isDuplicateModel) {
+        const rowError = {
+          data: row,
+          row: index + 2,
+          ...(missingKeys) && { missingKeys },
+          ...(invalidEntries) && { invalidEntries },
+          ...(isDuplicateModel) && { isDuplicateModel },
+          ...(invalidCalibration) && { invalidCalibration },
+        };
+        allRowErrors.push(rowError);
+      }
     });
-    const handleResponse = (response) => {
-      console.log('Query Response:');
-      console.log(response);
-    };
-    console.log('Sending query for bulk import...');
-    Query({
-      query,
-      queryName,
-      getVariables,
-      handleResponse,
-    });
+
+    console.log('All Row Errors: ');
+    console.log(allRowErrors);
+
+    // // Now all fields have been validated, time to attempt a db push...
+    // const query = print(IMPORT_MODELS);
+    // const queryName = 'bulkImportData';
+
+    // const getVariables = () => ({
+    //   data,
+    // });
+    // const handleResponse = (response) => {
+    //   console.log('Query Response:');
+    //   console.log(response);
+    // };
+    // console.log('Sending query for bulk import...');
+    // Query({
+    //   query,
+    //   queryName,
+    //   getVariables,
+    //   handleResponse,
+    // });
   };
 
   return (
