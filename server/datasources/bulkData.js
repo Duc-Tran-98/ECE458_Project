e39b72ab02877cdf8cdedd4124ddd4f5170e099a
Louis Jensen <lbj7@duke.edu>
2021-02-10 12:26:24 -0500

    update bulk import backend to use approved format

--- server/datasources/bulkData.js
+++ server/datasources/bulkData.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-operators */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable no-await-in-loop */
 // This file deals with what methods a model model should have
@@ -32,18 +33,16 @@ class BulkDataAPI extends DataSource {
   async bulkImportData({
     models,
     instruments,
-    calibrationEvents,
+    // calibrationEvents,
   }) {
     this.response = { success: true, errorList: [] };
     const storeModel = await this.store;
     this.store = storeModel;
-    // loop through models
-    // validate (make sure doesn't already exist) if ERROR delete all previous adds
-    // add
+
     let anyError = false;
     let addedModels = [];
     let addedInstruments = [];
-    let addedCalibrationIds = [];
+    // let addedCalibrationIds = [];
 
     await this.addModels(models).then(async (modelResponse) => {
       addedModels = modelResponse;
@@ -59,15 +58,15 @@ class BulkDataAPI extends DataSource {
       }
     });
 
-    await this.addCalibrationEvents(calibrationEvents).then(async (calibrationEventResponse) => {
-      addedCalibrationIds = calibrationEventResponse;
-      if (calibrationEvents.length !== calibrationEventResponse.length) {
-        anyError = true;
-      }
-    });
+    // await this.addCalibrationEvents(calibrationEvents).then(async (calibrationEventResponse) => {
+    //   addedCalibrationIds = calibrationEventResponse;
+    //   if (calibrationEvents.length !== calibrationEventResponse.length) {
+    //     anyError = true;
+    //   }
+    // });
 
     if (anyError) {
-      await this.deleteAddedCalibrationEvents(addedCalibrationIds);
+      // await this.deleteAddedCalibrationEvents(addedCalibrationIds);
       await this.deleteAddedInstruments(instruments, addedInstruments);
       await this.deleteAddedModels(models, addedModels);
       this.response.success = false;
@@ -75,43 +74,45 @@ class BulkDataAPI extends DataSource {
     return JSON.stringify(this.response);
   }
 
-  async addCalibrationEvents(calibrationEvents) {
-    // eslint-disable-next-line prefer-const
-    let added = [];
-    for (let i = 0; i < calibrationEvents.length; i += 1) {
-      const calibrationEvent = calibrationEvents[i];
-      const vendor = calibrationEvent.vendor;
-      const modelNumber = calibrationEvent.modelNumber;
-      const serialNumber = calibrationEvent.serialNumber;
-      const user = calibrationEvent.user;
-      const date = calibrationEvent.date;
-      const comment = calibrationEvent.comment;
-      const storeModel = await this.store;
-      this.store = storeModel;
-      await this.store.instruments.findAll({
-        where: { modelNumber, vendor, serialNumber },
-      }).then(async (instrument) => {
-        if (instrument && instrument[0]) {
-          if (!isValidDate(date)) { // checks if date is valid
-            this.response.errorList.push(`ERROR: Instrument ${vendor} ${modelNumber} ${serialNumber} Date must be in format YYYY-MM-DD`);
-            return;
-          }
-          const calibrationHistoryIdReference = instrument[0].dataValues.id;
-          const event = await this.store.calibrationEvents.create({
-            calibrationHistoryIdReference,
-            user,
-            date,
-            comment,
-          });
-          // console.log(event.dataValues.id);
-          added.push(event.dataValues.id);
-        } else {
-          this.response.errorList.push(`ERROR: Instrument ${vendor} ${modelNumber} ${serialNumber} does not exists`);
-        }
-      });
-    }
-    return added;
-  }
+  // async addCalibrationEvents(calibrationEvents) {
+  //   // eslint-disable-next-line prefer-const
+  //   let added = [];
+  //   for (let i = 0; i < calibrationEvents.length; i += 1) {
+  //     const calibrationEvent = calibrationEvents[i];
+  //     const vendor = calibrationEvent.vendor;
+  //     const modelNumber = calibrationEvent.modelNumber;
+  //     const serialNumber = calibrationEvent.serialNumber;
+  //     const user = calibrationEvent.user;
+  //     const date = calibrationEvent.date;
+  //     const comment = calibrationEvent.comment;
+  //     const storeModel = await this.store;
+  //     this.store = storeModel;
+  //     await this.store.instruments.findAll({
+  //       where: { modelNumber, vendor, serialNumber },
+  //     }).then(async (instrument) => {
+  //       if (instrument && instrument[0]) {
+  //         if (!isValidDate(date)) { // checks if date is valid
+  // eslint-disable-next-line max-len
+  //           this.response.errorList.push(`ERROR: Instrument ${vendor} ${modelNumber} ${serialNumber} Date must be in format YYYY-MM-DD`);
+  //           return;
+  //         }
+  //         const calibrationHistoryIdReference = instrument[0].dataValues.id;
+  //         const event = await this.store.calibrationEvents.create({
+  //           calibrationHistoryIdReference,
+  //           user,
+  //           date,
+  //           comment,
+  //         });
+  //         // console.log(event.dataValues.id);
+  //         added.push(event.dataValues.id);
+  //       } else {
+  // eslint-disable-next-line max-len
+  //         this.response.errorList.push(`ERROR: Instrument ${vendor} ${modelNumber} ${serialNumber} does not exists`);
+  //       }
+  //     });
+  //   }
+  //   return added;
+  // }
 
   async addInstruments(instruments) {
     // eslint-disable-next-line prefer-const
@@ -122,6 +123,22 @@ class BulkDataAPI extends DataSource {
       const modelNumber = currentInstrument.modelNumber;
       const serialNumber = currentInstrument.serialNumber;
       const comment = currentInstrument.comment;
+      const calibrationUser = currentInstrument.calibrationUser;
+      const calibrationDate = currentInstrument.calibrationDate;
+      const calibrationComment = currentInstrument.calibrationComment;
+      if (calibrationUser == null && calibrationDate != null
+        || calibrationUser != null && calibrationDate == null) {
+        this.response.errorList.push(`ERROR: Calibration event for instrument ${vendor} ${modelNumber} ${serialNumber} must have user and date`);
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+      if (calibrationDate != null && !isValidDate(calibrationDate)) {
+        this.response.errorList.push(`ERROR: Instrument ${vendor} ${modelNumber} ${serialNumber} Date must be in format YYYY-MM-DD`);
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
       await this.store.models.findAll({ where: { modelNumber, vendor } }).then(async (model) => {
         if (model && model[0]) {
           await this.getInstrument({
@@ -132,9 +149,10 @@ class BulkDataAPI extends DataSource {
             } else {
               const modelReference = model[0].dataValues.id;
               // eslint-disable-next-line prefer-destructuring
-              const calibrationFrequency = model[0].dataValues.calibrationFrequency;
+              // const calibrationFrequency = model[0].dataValues.calibrationFrequency;
+              const { description, calibrationFrequency } = model[0].dataValues;
               const isCalibratable = (calibrationFrequency > 0);
-              await this.store.instruments.create({
+              const inst = await this.store.instruments.create({
                 modelReference,
                 vendor,
                 modelNumber,
@@ -142,7 +160,18 @@ class BulkDataAPI extends DataSource {
                 isCalibratable,
                 comment,
                 calibrationFrequency,
+                description,
               });
+              const calibrationHistoryIdReference = inst.dataValues.id;
+
+              if (calibrationUser != null) {
+                await this.store.calibrationEvents.create({
+                  calibrationHistoryIdReference,
+                  user: calibrationUser,
+                  date: calibrationDate,
+                  comment: calibrationComment,
+                });
+              }
               added.push(i);
             }
           });
@@ -154,12 +183,12 @@ class BulkDataAPI extends DataSource {
     return added;
   }
 
-  async deleteAddedCalibrationEvents(ids) {
-    // eslint-disable-next-line no-restricted-syntax
-    for await (const id of ids) {
-      await this.store.calibrationEvents.destroy({ where: { id } });
-    }
-  }
+  // async deleteAddedCalibrationEvents(ids) {
+  //   // eslint-disable-next-line no-restricted-syntax
+  //   for await (const id of ids) {
+  //     await this.store.calibrationEvents.destroy({ where: { id } });
+  //   }
+  // }
 
   async deleteAddedInstruments(instruments, indices) {
     // eslint-disable-next-line no-restricted-syntax
