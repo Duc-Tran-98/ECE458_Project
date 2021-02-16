0f05ed68f1837a750e740514791823e2aa9e1849
Louis Jensen <lbj7@duke.edu>
2021-02-15 21:31:09 -0500

    bug fix: add calibration comment to by row export

--- client/src/pages/ListInstruments.jsx
+++ client/src/pages/ListInstruments.jsx
@@ -244,7 +244,7 @@ export default function ListInstruments() {
       serialNumber: element.serialNumber,
       comment: element.comment,
       calibrationDate: element.date,
-      calibrationComment: element.calibComment,
+      calibrationComment: element.calibrationComment,
     }));
     return filteredRows;
   };
