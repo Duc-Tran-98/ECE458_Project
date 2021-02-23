6fd606336cc766c908d7bdbcaad56f6e2a0a152d
Angel Huizar <ah426@duke.edu>
2021-02-22 17:52:00 -0800

    fixed history problem; now have smooth transition between table configs via urls; next, work on detail model view

--- client/src/pages/ListModels.jsx
+++ client/src/pages/ListModels.jsx
@@ -20,16 +20,15 @@ function ListModels() {
   const rowCount = parseInt(urlParams.get('count'), 10);
   const [initPage, setInitPage] = useState(parseInt(urlParams.get('page'), 10));
   const [initLimit, setInitLimit] = useState(parseInt(urlParams.get('limit'), 10));
-  // history.listen((location, action) => {
-  //   const urlVals = new URLSearchParams(location.search);
-  //   const lim = parseInt(urlVals.get('limit'), 10);
-  //   const pg = parseInt(urlVals.get('page'), 10);
-  //   console.log(action);
-  //   if ((action === 'PUSH' && lim === 25 && pg === 1) || action === 'POP') { // if user clicks on models nav link or goes back
-  //     setInitLimit(lim);
-  //     setInitPage(pg);
-  //   }
-  // });
+  history.listen((location, action) => {
+    const urlVals = new URLSearchParams(location.search);
+    const lim = parseInt(urlVals.get('limit'), 10);
+    const pg = parseInt(urlVals.get('page'), 10);
+    if ((action === 'PUSH' && lim === 25 && pg === 1) || action === 'POP') { // if user clicks on models nav link or goes back
+      setInitLimit(lim);
+      setInitPage(pg);
+    }
+  });
 
   const cellHandler = (e) => {
     if (e.field === 'view') {
@@ -139,14 +138,20 @@ function ListModels() {
         initPage={initPage}
         initLimit={initLimit}
         onPageChange={(page, limit) => {
-          history.push(`/viewModels?page=${page}&limit=${limit}&count=${rowCount}`);
-          setInitLimit(limit);
-          setInitPage(page);
+          const searchString = `?page=${page}&limit=${limit}&count=${rowCount}`;
+          if (window.location.search !== searchString) {
+            history.push(`/viewModels${searchString}`);
+            setInitLimit(limit);
+            setInitPage(page);
+          }
         }}
         onPageSizeChange={(page, limit) => {
-          history.push(`/viewModels?page=${page}&limit=${limit}&count=${rowCount}`);
-          setInitLimit(limit);
-          setInitPage(page);
+          const searchString = `?page=${page}&limit=${limit}&count=${rowCount}`;
+          if (window.location.search !== searchString) {
+            history.push(`/viewModels${searchString}`);
+            setInitLimit(limit);
+            setInitPage(page);
+          }
         }}
         rowCount={rowCount}
         cellHandler={cellHandler}
