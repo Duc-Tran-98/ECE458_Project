079aa329a6c7f75fc0b1254d164d816a082d5415
Max Smith <ms724@duke.edu>
2021-02-28 12:02:39 -0500

    Completed pass form with clear on submit

--- client/src/pages/UserInfo.jsx
+++ client/src/pages/UserInfo.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import UserContext from '../components/UserContext';
 import PasswordForm from '../components/PasswordForm';
+import 'react-toastify/dist/ReactToastify.css';
+import '../css/customToast.css';
 
 export default function UserInfo() {
   const user = useContext(UserContext);
@@ -10,13 +13,15 @@ export default function UserInfo() {
 
   const fullName = `${firstName} ${lastName}`;
 
-  const handleSubmitPassword = (oldPassword, newPassword) => {
-    console.log(`oldPassword: ${oldPassword}`);
-    console.log(`newPassword: ${newPassword}`);
+  // TODO: submit password to database
+  const handleSubmitPassword = (currentPassword, newPassword) => {
+    console.log(`handleSubmitPassword with current: ${currentPassword}\tnew: ${newPassword}`);
+    toast.success('Successfully updated password!');
   };
 
   return (
     <>
+      <ToastContainer />
       <h1>{`${fullName}'s Profile`}</h1>
       <PasswordForm handleSubmitPassword={handleSubmitPassword} />
     </>
