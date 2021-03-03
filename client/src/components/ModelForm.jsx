988f43caa0a1be1e921e043da2797705f1375da0
Max Smith <ms724@duke.edu>
2021-03-03 18:07:50 -0500

    Refactoring model form with yup schema and formik validation

--- client/src/components/ModelForm.jsx
+++ client/src/components/ModelForm.jsx
@@ -3,6 +3,8 @@ import Form from 'react-bootstrap/Form';
 import PropTypes from 'prop-types';
 import { gql } from '@apollo/client';
 import { print } from 'graphql';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import AsyncSuggest from './AsyncSuggest';
 
 const GET_MODELS_QUERY = gql`
@@ -14,6 +16,14 @@ const GET_MODELS_QUERY = gql`
 `;
 const query = print(GET_MODELS_QUERY);
 const queryName = 'getUniqueVendors';
+
+const schema = yup.object().shape({
+  modelNumber: yup.string().required(),
+  vendor: yup.string().required(),
+  calibrationFrequency: yup.int(),
+  comment: yup.string(),
+  description: yup.string(),
+});
 
 export default function ModelForm({
   modelNumber, vendor, calibrationFrequency, comment, description, handleSubmit, validated, changeHandler, viewOnly, onInputChange, diffSubmit,
@@ -40,110 +50,133 @@ export default function ModelForm({
   const formatSelected = (option, value) => option.vendor === value.vendor;
   const val = { vendor };
   return (
-    <Form
-      className="needs-validation"
-      noValidate
-      validated={validated}
-      onSubmit={handleSubmit}
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        modelNumber,
+        vendor,
+        calibrationFrequency,
+        comment,
+        description,
+      }}
     >
-      <div className="row mx-3">
-        <div className="col mt-3">
-          <Form.Group controlId="formModelNumber">
-            <Form.Label className="h4">Model Number</Form.Label>
-            <Form.Control
-              name="modelNumber"
-              type="text"
-              required
-              value={modelNumber.value}
-              onChange={changeHandler}
-              disabled={disabled}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter a Model Number.
-            </Form.Control.Feedback>
-          </Form.Group>
-        </div>
-        <div className="col mt-3">
-          <Form.Group>
-            <Form.Label className="h4">Vendor</Form.Label>
-            {viewOnly ? (
-              <Form.Control
-                type="text"
-                name="modelSelection"
-                value={`${vendor}`}
-                onChange={changeHandler}
-                disabled={disabled}
-              />
-            ) : (
-              <AsyncSuggest
-                query={query}
-                queryName={queryName}
-                onInputChange={onInputChange}
-                label="Choose a vendor"
-                getOptionSelected={formatSelected}
-                getOptionLabel={formatOption}
-                value={val}
-                allowAdditions
-              />
-            )}
-          </Form.Group>
-        </div>
-      </div>
-      <div className="row mx-3 border-top border-dark mt-3">
-        <div className="col mt-3">
-          <Form.Group controlId="formCalibrationFrequency">
-            <Form.Label className="h4 text-nowrap ">
-              Calibration Frequency
-            </Form.Label>
-            <Form.Control
-              type="number"
-              min={0}
-              name="calibrationFrequency"
-              value={calibrationFrequency.value}
-              onChange={changeHandler}
-              disabled={disabled}
-            />
-          </Form.Group>
-        </div>
-        <div className="col mt-3">
-          <Form.Group controlId="formDescription">
-            <Form.Label className="h4">Description</Form.Label>
-            <Form.Control
-              type="text"
-              required
-              name="description"
-              value={description.value}
-              onChange={changeHandler}
-              disabled={disabled}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter a valid description.
-            </Form.Control.Feedback>
-          </Form.Group>
-        </div>
-      </div>
-      <div className="row mx-3 border-top border-dark mt-3">
-        <div className="col mt-3">
-          <Form.Group controlId="formComment">
-            <Form.Label className="h4">Comment</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              name="comment"
-              value={comment.value}
-              onChange={changeHandler}
-              disabled={disabled}
-            />
-          </Form.Group>
-        </div>
-      </div>
-      {((typeof viewOnly === 'undefined' || !viewOnly) && !diffSubmit) && (
-        <div className="d-flex justify-content-center mt-3 mb-3">
-          <button type="submit" className="btn ">
-            Add Model
-          </button>
-        </div>
+      {/* {
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors,
+      } */}
+      {() => (
+        <Form
+          className="needs-validation"
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+        >
+          <div className="row mx-3">
+            <div className="col mt-3">
+              <Form.Group controlId="formModelNumber">
+                <Form.Label className="h4">Model Number</Form.Label>
+                <Form.Control
+                  name="modelNumber"
+                  type="text"
+                  required
+                  value={modelNumber}
+                  onChange={changeHandler}
+                  disabled={disabled}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a Model Number.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </div>
+            <div className="col mt-3">
+              <Form.Group>
+                <Form.Label className="h4">Vendor</Form.Label>
+                {viewOnly ? (
+                  <Form.Control
+                    type="text"
+                    name="modelSelection"
+                    value={`${vendor}`}
+                    onChange={changeHandler}
+                    disabled={disabled}
+                  />
+                ) : (
+                  <AsyncSuggest
+                    query={query}
+                    queryName={queryName}
+                    onInputChange={onInputChange}
+                    label="Choose a vendor"
+                    getOptionSelected={formatSelected}
+                    getOptionLabel={formatOption}
+                    value={val}
+                    allowAdditions
+                  />
+                )}
+              </Form.Group>
+            </div>
+          </div>
+          <div className="row mx-3 border-top border-dark mt-3">
+            <div className="col mt-3">
+              <Form.Group controlId="formCalibrationFrequency">
+                <Form.Label className="h4 text-nowrap ">
+                  Calibration Frequency
+                </Form.Label>
+                <Form.Control
+                  type="number"
+                  min={0}
+                  name="calibrationFrequency"
+                  value={calibrationFrequency}
+                  onChange={changeHandler}
+                  disabled={disabled}
+                />
+              </Form.Group>
+            </div>
+            <div className="col mt-3">
+              <Form.Group controlId="formDescription">
+                <Form.Label className="h4">Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  required
+                  name="description"
+                  value={description}
+                  onChange={changeHandler}
+                  disabled={disabled}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid description.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </div>
+          </div>
+          <div className="row mx-3 border-top border-dark mt-3">
+            <div className="col mt-3">
+              <Form.Group controlId="formComment">
+                <Form.Label className="h4">Comment</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  name="comment"
+                  value={comment}
+                  onChange={changeHandler}
+                  disabled={disabled}
+                />
+              </Form.Group>
+            </div>
+          </div>
+          {((typeof viewOnly === 'undefined' || !viewOnly) && !diffSubmit) && (
+          <div className="d-flex justify-content-center mt-3 mb-3">
+            <button type="submit" className="btn ">
+              Add Model
+            </button>
+          </div>
+          )}
+        </Form>
       )}
-    </Form>
+    </Formik>
   );
 }
