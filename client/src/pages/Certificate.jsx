21497233f622b23cb64e5f50158c1f69644769bf
Natasha von Seelen <nlv10@duke.edu>
2021-03-08 22:01:51 -0500

    links have bug

--- client/src/pages/Certificate.jsx
+++ client/src/pages/Certificate.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  // eslint-disable-next-line no-unused-vars
   PDFViewer, Document, Page, Text, Image, View, StyleSheet, Link,
 } from '@react-pdf/renderer';
 import GetCalibHistory from '../queries/GetCalibHistory';
@@ -94,18 +95,26 @@ function MyCertificate() {
   const calibrationDate = window.sessionStorage.getItem('calibrationDate');
   const expirationDate = strftime('%F', new Date(window.sessionStorage.getItem('expirationDate')));
   const comment = window.sessionStorage.getItem('calibComment');
-  const id = window.sessionStorage.getItem('id');
+  let id = window.sessionStorage.getItem('id');
+  id = parseInt(id, 10);
 
   function getURLExtension(url) {
     return url.split(/[#?]/)[0].split('.').pop().trim();
   }
 
   const [calibEvent, setCalibEvent] = React.useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const [fetched, setHasFetched] = React.useState(false);
+
   React.useEffect(() => {
     let active = true;
     (() => {
       if (active) {
-        GetCalibHistory({ id, mostRecent: true }).then((data) => setCalibEvent(data));
+        GetCalibHistory({ id, mostRecent: true }).then((data) => {
+          setCalibEvent(data);
+          console.log(data);
+          setHasFetched(true);
+        });
       }
     })();
     return () => {
@@ -115,113 +124,132 @@ function MyCertificate() {
 
   console.log(calibEvent);
 
-  const evidenceFile = 'https://people.duke.edu/~tkb13/courses/ece458/ev2.pdf';
-  const evidenceFileType = getURLExtension(evidenceFile);
+  const urlPath = process.env.NODE_ENV.includes('dev')
+    ? `http://localhost:3000/data/${calibEvent?.fileLocation}`
+    : `https://hpt.hopto.org/data/${calibEvent?.fileLocation}`;
+  const evidenceFile = calibEvent ? urlPath : '';
+  const evidenceFileType = calibEvent ? getURLExtension(evidenceFile) : '';
+
+  console.log(evidenceFileType);
+  console.log(evidenceFile);
+
+  // eslint-disable-next-line no-unused-vars
+  const displayLink = (((evidenceFileType === 'pdf') || (evidenceFileType === 'xlsx'))) ? (
+    <Text style={styles.largeText}>
+      {'\n'}
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */ }
+      <Link src={evidenceFile}> View Evidence </Link>
+      {/* <a href={`/data/${calibEvent?.fileLocation}`} rel="noreferrer" target="_blank">link title</a> */}
+      {'\n'}
+    </Text>
+  ) : (null);
+
+  console.log(displayLink);
+
+  // eslint-disable-next-line no-unused-vars
+  const displayImage = ((evidenceFileType === 'jpg') || (evidenceFileType === 'png') || (evidenceFileType === 'gif')) ? (
+    <View style={styles.centerView}>
+      <Image style={styles.image} src={evidenceFile} />
+    </View>
+  ) : (null);
+
+  console.log(displayImage);
 
   return (
     <Document>
       <Page style={styles.page} size="LETTER">
-        <View style={styles.outerBorder}>
-          <View style={styles.innerBorder}>
+        <>
+          {fetched && (
 
-            <View style={styles.centerView}>
-              <Image style={styles.logo} src="HPT_logo.png" />
-            </View>
+          <View style={styles.outerBorder}>
+            <View style={styles.innerBorder}>
 
-            <Text style={styles.title}>
-              Certificate of Calibration
-              {'\n\n'}
-            </Text>
-
-            <View style={styles.columnView}>
-              <View style={styles.rightColumn}>
-                <Text style={styles.largeText}>
-                  Vendor:
-                  {' '}
-                  {vendor}
-                </Text>
-                <Text style={styles.largeText}>
-                  Model Number:
-                  {' '}
-                  {model}
-                </Text>
-              </View>
-              <View style={styles.leftColumn}>
-                <Text style={styles.largeText}>
-                  Serial Number:
-                  {' '}
-                  {serial}
-                </Text>
-                <Text style={styles.largeText}>
-                  Asset Tag:
-                  {' '}
-                  {assetTag}
-                </Text>
-              </View>
-            </View>
-
-            <Text style={styles.largeText}>
-              {'\n'}
-              Model Description:
-            </Text>
-            <Text style={styles.smallText}>
-              {description}
-            </Text>
-
-            <Text style={styles.largeText}>
-              {'\n'}
-              Comment:
-            </Text>
-            <Text style={styles.smallText}>
-              {comment}
-            </Text>
-
-            <View style={styles.columnView}>
-              <View style={styles.rightColumn}>
-                <Text style={styles.largeText}>
-                  {'\n'}
-                  Calibrated By:
-                  {' '}
-                  {name}
-                </Text>
-                <Text style={styles.largeText}>
-                  Username:
-                  {' '}
-                  {username}
-                </Text>
+              <View style={styles.centerView}>
+                <Image style={styles.logo} src="HPT_logo.png" />
               </View>
 
-              <View style={styles.leftColumn}>
-                <Text style={styles.largeText}>
-                  {'\n'}
-                  Date of Calibration:
-                  {' '}
-                  {calibrationDate}
-                </Text>
-                <Text style={styles.largeText}>
-                  Date of Expiration:
-                  {' '}
-                  {expirationDate}
-                </Text>
+              <Text style={styles.title}>
+                Certificate of Calibration
+                {'\n\n'}
+              </Text>
+
+              <View style={styles.columnView}>
+                <View style={styles.rightColumn}>
+                  <Text style={styles.largeText}>
+                    Vendor:
+                    {' '}
+                    {vendor}
+                  </Text>
+                  <Text style={styles.largeText}>
+                    Model Number:
+                    {' '}
+                    {model}
+                  </Text>
+                </View>
+                <View style={styles.leftColumn}>
+                  <Text style={styles.largeText}>
+                    Serial Number:
+                    {' '}
+                    {serial}
+                  </Text>
+                  <Text style={styles.largeText}>
+                    Asset Tag:
+                    {' '}
+                    {assetTag}
+                  </Text>
+                </View>
               </View>
-            </View>
 
-            {((evidenceFileType === 'pdf') || (evidenceFileType === 'xlsx')) && (
-            <Text style={styles.largeText}>
-              {'\n'}
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */ }
-              <Link src={evidenceFile}> View Evidence </Link>
-              {'\n'}
-            </Text>
-            )}
+              <Text style={styles.largeText}>
+                {'\n'}
+                Model Description:
+              </Text>
+              <Text style={styles.smallText}>
+                {description}
+              </Text>
 
-            {((evidenceFileType === 'jpg') || (evidenceFileType === 'png') || (evidenceFileType === 'gif')) && (
-            <View style={styles.centerView}>
-              <Image style={styles.image} src={evidenceFile} />
-            </View>
-            )}
+              <Text style={styles.largeText}>
+                {'\n'}
+                Comment:
+              </Text>
+              <Text style={styles.smallText}>
+                {comment}
+              </Text>
 
-            {(evidenceFileType === 'load_bank') && (
+              <View style={styles.columnView}>
+                <View style={styles.rightColumn}>
+                  <Text style={styles.largeText}>
+                    {'\n'}
+                    Calibrated By:
+                    {' '}
+                    {name}
+                  </Text>
+                  <Text style={styles.largeText}>
+                    Username:
+                    {' '}
+                    {username}
+                  </Text>
+                </View>
+
+                <View style={styles.leftColumn}>
+                  <Text style={styles.largeText}>
+                    {'\n'}
+                    Date of Calibration:
+                    {' '}
+                    {calibrationDate}
+                  </Text>
+                  <Text style={styles.largeText}>
+                    Date of Expiration:
+                    {' '}
+                    {expirationDate}
+                  </Text>
+                </View>
+              </View>
+
+              {displayLink}
+              {displayImage}
+
+              {(evidenceFileType === 'load_bank') && (
               <View style={styles.table}>
                 {' '}
                 {/* TableHeader */}
@@ -285,12 +313,16 @@ function MyCertificate() {
                 </View>
                 {' '}
               </View>
-            )}
-
+              )}
+            </View>
           </View>
-        </View>
+          )}
+
+        </>
       </Page>
+
     </Document>
+
   );
 }
 
