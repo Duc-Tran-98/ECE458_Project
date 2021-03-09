3d6f435f424f008aaff7833ce697b4d10997c74e
Max Smith <ms724@duke.edu>
2021-03-08 19:51:34 -0500

    Refactoring impinstruments

--- client/src/components/ImpInstruments.jsx
+++ client/src/components/ImpInstruments.jsx
@@ -1,53 +1,277 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import { camelCase } from 'lodash';
+import { gql } from '@apollo/client';
+import { print } from 'graphql';
+import moment from 'moment';
+import Query from './UseQuery';
 import CustomUpload from './CustomUpload';
+import ModalAlert from './ModalAlert';
+import ImportModelError from './ImportModelError';
+import DisplayGrid from './UITable';
+import UserContext from './UserContext';
 
 export default function ImpInstruments() {
-  const handleImport = (fileInfo) => {
-    console.log(fileInfo);
+  const user = useContext(UserContext);
+  const [rows, setRow] = React.useState([]);
+  const [showTable, setShowTable] = React.useState(false);
+  const [importStatus, setImportStatus] = React.useState('Import');
+  const [show, setShow] = React.useState(false);
+  const [allRowErrors, setAllRowErrors] = React.useState([]);
+  const closeModal = () => {
+    setShow(false);
+    setAllRowErrors([]);
   };
 
   const requiredHeaders = [
     { display: 'Vendor', value: 'vendor' },
     { display: 'Model-Number', value: 'modelNumber' },
-    { display: 'Short-Description', value: 'description' },
+    { display: 'Serial-Number', value: 'serialNumber' },
+    { display: 'Asset-Tag-Number', value: 'assetTag' },
     { display: 'Comment', value: 'comment' },
-    { display: 'Model-Categories', value: 'categories' },
-    { display: 'Load-Bank-Support', value: 'loadBankSupport' },
-    { display: 'Calibration-Frequency', value: 'calibrationFrequency' },
+    { display: 'Calibration-Date', value: 'calibrationDate' },
+    { display: 'Calibration-Comment', value: 'calibrationComment' },
+    { display: 'Instrument-Categories', value: 'instrumentCategories' },
   ];
-
   const customHeaderTransform = (header) => {
     switch (header) {
       case 'Short-Description':
         return 'description';
-      case 'Model-Categories':
+      case 'Instrument-Categories':
         return 'categories';
+      case 'Asset-Tag-Number':
+        return 'assetTag';
       default:
         return camelCase(header);
     }
   };
-
+  // TODO: Remove transform here, perform after error handling
+  // TODO: Submit variance request to allow these (load bank likely, calib unlikely)
   const customTransform = (value, header) => {
     switch (header) {
       case 'categories':
-        return value.split(/\s+/);
+        // eslint-disable-next-line no-case-declarations
+        const arr = value.trim().split(/\s+/);
+        if (arr.length > 0 && arr[0] !== '') { return arr; }
+        return null;
       default:
-        return value.trim();
+        return value.trim().length > 0 ? value.trim() : null;
     }
   };
-
   const uploadLabel = 'Select Instruments File';
+
+  const IMPORT_INSTRUMENTS = gql`
+    mutation ImportInstruments (
+      $filteredData: [InstrumentInput]!
+    ) {
+      bulkImportInstruments(instruments: $filteredData)
+    }
+    `;
+  const query = print(IMPORT_INSTRUMENTS);
+  const queryName = 'bulkImportInstruments';
+  const renderTable = (instruments) => {
+    const filteredData = instruments.map((obj) => ({
+      id: String(obj.vendor + obj.modelNumber),
+      vendor: String(obj.vendor),
+      modelNumber: String(obj.modelNumber),
+      description: String(obj.description),
+      ...(obj.comment) && { comment: String(obj.comment) },
+      ...(obj.calibrationFrequency !== 'N/A') && { calibrationFrequency: parseInt(obj.calibrationFrequency, 10) },
+    }));
+    setRow(filteredData);
+    setShowTable(true);
+    console.log(filteredData);
+  };
+
+  const characterLimits = {
+    instrument: {
+      vendor: 30,
+      modelNumber: 40,
+      serialNumber: 40,
+      comment: 2000,
+      calibrationDate: 20,
+      calibrationComment: 2000,
+    },
+  };
+  const cols = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      width: 60,
+      hide: true,
+      disableColumnMenu: true,
+      type: 'number',
+    },
+    { field: 'vendor', headerName: 'Vendor', width: 120 },
+    { field: 'modelNumber', headerName: 'Model-Number', width: 150 },
+    { field: 'serialNumber', headerName: 'Serial-Number', width: 150 },
+    {
+      field: 'comment',
+      headerName: 'Comment',
+      width: 300,
+      renderCell: (params) => (
+        <div className="overflow-auto">
+          {params.value}
+        </div>
+      ),
+    },
+    { field: 'comment', headerName: 'Comment', width: 250 },
+    { field: 'calibrationUser', headerName: 'Calib-User', width: 150 },
+    { field: 'calibrationDate', headerName: 'Calib-Date', width: 150 },
+    {
+      field: 'calibrationComment',
+      headerName: 'Calibration Comment',
+      width: 300,
+      hide: true,
+      renderCell: (params) => (
+        <div className="overflow-auto">
+          {params.value}
+        </div>
+      ),
+    },
+
+  ];
+
+  const getMissingKeys = (row) => {
+    const missingKeys = [];
+    if (!row.vendor) missingKeys.push('Vendor');
+    if (!row.modelNumber) missingKeys.push('Model-Number');
+    if (!row.serialNumber) missingKeys.push('Serial-Number');
+    return missingKeys.length > 0 ? missingKeys : null;
+  };
+
+  const checkDuplicateInstrument = (data, vendor, modelNumber, serialNumber, myIndex) => {
+    let isDuplicateInstrument = false;
+    data.forEach((row, index) => {
+      if (index !== myIndex && row.vendor === vendor && row.modelNumber === modelNumber && row.serialNumber === serialNumber) {
+        isDuplicateInstrument = true;
+      }
+    });
+    return isDuplicateInstrument;
+  };
+
+  const validateRow = (row) => {
+    const invalidKeys = [];
+    if (row.vendor && row.vendor.length > characterLimits.instrument.vendor) { invalidKeys.push('Vendor'); }
+    if (row.modelNumber && row.modelNumber.length > characterLimits.instrument.modelNumber) { invalidKeys.push('Model-Number'); }
+    if (row.serialNumber && row.serialNumber.length > characterLimits.instrument.serialNumber) { invalidKeys.push('Serial-Number'); }
+    if (row.comment && row.comment.length > characterLimits.instrument.comment) { invalidKeys.push('Comment'); }
+    if (row.calibrationDate && row.calibrationDate.length > characterLimits.instrument.calibrationDate) { invalidKeys.push('Calibration-Date'); }
+    if (row.calibrationComment && row.calibrationComment.length > characterLimits.instrument.calibrationComment) { invalidKeys.push('Calibration-Comment'); }
+    return invalidKeys.length > 0 ? invalidKeys : null;
+  };
+
+  const emptyLine = (obj) => !Object.values(obj).every((x) => x == null);
+
+  const getImportErrors = (fileInfo) => {
+    const importRowErrors = [];
+    fileInfo.forEach((row, index) => {
+      if (!emptyLine(row)) {
+        // Check missing keys
+        const missingKeys = getMissingKeys(row);
+
+        let isDuplicateInstrument;
+        if (row.vendor && row.modelNumber && row.serialNumber) {
+          isDuplicateInstrument = checkDuplicateInstrument(fileInfo, row.vendor, row.modelNumber, row.serialNumber, index);
+        }
+
+        // Validate entries by length
+        const invalidEntries = validateRow(row);
+
+        // Validate calibration date (missing, form, future)
+
+        // If any errors exist, create errors object
+        if (missingKeys || invalidEntries || isDuplicateInstrument) {
+          const rowError = {
+            data: row,
+            row: index + 2,
+            ...(missingKeys) && { missingKeys },
+            ...(invalidEntries) && { invalidEntries },
+            ...(isDuplicateInstrument) && { isDuplicateInstrument },
+          };
+          importRowErrors.push(rowError);
+        }
+      }
+    });
+    return importRowErrors.length > 0 ? importRowErrors : null;
+  };
+
+  const validateFile = (fileInfo) => {
+    const importRowErrors = getImportErrors(fileInfo);
+    if (importRowErrors) {
+      console.log('Errors in file validation: ');
+      console.log(importRowErrors);
+      setAllRowErrors(importRowErrors);
+      setShow(true);
+      return false;
+    }
+    return true;
+  };
+
+  // TODO: Implement trim with transform
+  // const trimEmptyLines = (fileInfo) => fileInfo.filter((row) => {
+  //   console.log(row);
+  //   console.log(`is empty?: ${!emptyLine(row)}`);
+  //   return !emptyLine(row);
+  // });
+
+  const filterData = (fileInfo) => fileInfo.map((obj) => ({
+    vendor: String(obj.vendor),
+    modelNumber: String(obj.modelNumber),
+    serialNumber: String(obj.serialNumber),
+    ...(obj.comment) && { comment: String(obj.comment) },
+    ...(obj.calibrationDate) && { calibrationUser: user.userName },
+    ...(obj.calibrationDate) && { calibrationDate: moment(obj.calibrationDate, 'MM/DD/YYYY').format('YYYY-MM-DD') },
+    ...(obj.calibrationDate && obj.calibrationComment) && { calibrationComment: obj.calibrationComment },
+  }));
+
+  const handleImport = (fileInfo, resetUpload) => {
+    console.log('Handling import with fileInfo: ');
+    console.log(fileInfo);
+    // const models = trimEmptyLines(fileInfo);
+    const instruments = filterData(fileInfo);
+    console.log(instruments);
+    setImportStatus('Validating');
+    if (!validateFile(instruments)) {
+      setImportStatus('Import');
+      return;
+    }
+
+    // File has been validated, now push to database
+    const getVariables = () => ({ instruments });
+    Query({
+      query,
+      queryName,
+      getVariables,
+      handleResponse: (response) => {
+        console.log(response);
+        if (response.success) {
+          toast.success(`Successfully imported ${instruments.length} instruments!`);
+          renderTable(instruments);
+          resetUpload();
+        } else {
+          toast.error(response.message);
+        }
+        setImportStatus('Import');
+      },
+    });
+  };
 
   return (
     <>
+      <ToastContainer />
       <CustomUpload
         requiredHeaders={requiredHeaders}
         customHeaderTransform={customHeaderTransform}
         customTransform={customTransform}
         uploadLabel={uploadLabel}
         handleImport={handleImport}
+        importStatus={importStatus}
       />
+      <ModalAlert handleClose={closeModal} show={show} title="Error Importing Instruments" width=" ">
+        <ImportModelError allRowErrors={allRowErrors} errorList={[]} />
+      </ModalAlert>
+      {showTable && <DisplayGrid rows={rows} cols={cols} />}
     </>
   );
 }
