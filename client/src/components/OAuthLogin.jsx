ed10a4a625b9dee278eb006acb3e74bb5d4430e0
Max Smith <ms724@duke.edu>
2021-03-10 00:51:56 -0500

    Bootstrapping oauth login

--- client/src/components/OAuthLogin.jsx
+++ client/src/components/OAuthLogin.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
+const PROD_URI = 'https://hpt.hopto.org/oauth/consume';
+const CLIENT_ID = 'ece458_2021_s_ms724';
 const myRedirectURI = process.env.NODE_ENV.includes('dev')
   ? process.env.REACT_APP_OAUTH_REDIRECT_URI_DEV
-  : process.env.REACT_APP_OAUTH_REDIRECT_URI_PROD;
+  : PROD_URI;
 
 export default function OAuthLogin() {
   const getAuthCodeRequest = () => {
-    const baseURL = process.env.REACT_APP_OAUTH_REQUEST_URL ? process.env.REACT_APP_OAUTH_REQUEST_URL : 'https://oauth.oit.duke.edu/oidc/authorize';
+    const baseURL = 'https://oauth.oit.duke.edu/oidc/authorize';
     const redirectURI = encodeURI(myRedirectURI);
-    const requestURL = `${baseURL}?client_id=${process.env.REACT_APP_OAUTH_CLIENT_ID}&redirect_uri=${redirectURI}&response_type=code`;
+    const requestURL = `${baseURL}?client_id=${CLIENT_ID}&redirect_uri=${redirectURI}&response_type=code`;
     return requestURL;
   };
 
   const submitAuthCodeRequest = (authCodeRequest) => {
-    console.log(authCodeRequest);
-    // window.location.href = authCodeRequest;
+    window.location.href = authCodeRequest;
   };
 
   const handleOAuth = () => {
-    console.log('Handling OAuth login with process env: ');
-    console.log(process.env);
-
     // Get Auth Code
     const authCodeRequest = getAuthCodeRequest();
     console.log(authCodeRequest);
