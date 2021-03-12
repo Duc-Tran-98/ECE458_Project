40352641464b47f6700395f43e2ea6bd52e2e698
Duc Tran <dt135@duke.edu>
2021-03-11 19:06:28 -0500

    Test Works within the containers

--- server/datasources/__tests__/models.test.js
+++ server/datasources/__tests__/models.test.js
@@ -1,48 +1,48 @@
 const ModelAPI = require('../models');
-const UserAPI = require('../users');
-const InstrumentAPI = require('../instruments');
-const CalibrationEventAPI = require('../calibrationEvents');
-const BulkDataAPI = require('../bulkData');
 const { createStore } = require('../../util');
 
 describe('Test Model Creation and Querying', () => {
-  const store = createStore();
-  const dataSources = () => ({
-    userAPI: new UserAPI({ store }),
-    modelAPI: new ModelAPI({ store }),
-    instrumentAPI: new InstrumentAPI({ store }),
-    calibrationEventAPI: new CalibrationEventAPI({ store }),
-    bulkDataAPI: new BulkDataAPI({ store }),
-  });
+  // const store = await createStore();
+  // console.log(store);
+  // const dataSources = () => ({
+  //   userAPI: new UserAPI({ store }),
+  //   modelAPI: new ModelAPI({ store }),
+  //   instrumentAPI: new InstrumentAPI({ store }),
+  //   calibrationEventAPI: new CalibrationEventAPI({ store }),
+  //   bulkDataAPI: new BulkDataAPI({ store }),
+  // });
+  // beforeAll(async () => {
+  //   const store = await createStore();
+  //   await store.db.sync({ force: true });
+  // });
+
+  const testModel = {
+    modelNumber: 'Duke',
+    vendor: 'Voltmeter',
+    description: 'This is a test voltmeter',
+    comment: 'This model is for testing purposes',
+    calibrationFrequency: 30,
+    supportLoadBankCalibration: true,
+  };
 
   it('Should succeed in creating a new model with valid inputs', async () => {
-    const testModel = {
-      modelNumber: 'Duke',
-      vendor: 'Voltmeter',
-      description: 'This is a test voltmeter',
-      comment: 'This model is for testing purposes',
-      calibrationFrequency: 30,
-      supportLoadBankCalibration: true,
-    };
-    const response = await dataSources.modelAPI.addModel(testModel);
-    expect(response).toMatchObject({
+    const store = await createStore();
+    const modelAPI = new ModelAPI({ store });
+
+    const response = await modelAPI.addModel(testModel);
+    expect(response).toMatch(JSON.stringify({
       message: `Added new model, ${testModel.vendor} ${testModel.modelNumber}, into the DB!`,
       success: true,
-    });
+    }));
   });
   it('Should fail on trying to create a duplicate model', async () => {
-    const testModel = {
-      modelNumber: 'Duke',
-      vendor: 'Voltmeter',
-      description: 'This is a test voltmeter',
-      comment: 'This model is for testing purposes',
-      calibrationFrequency: 30,
-      supportLoadBankCalibration: true,
-    };
-    const response = await dataSources.modelAPI.addModel(testModel);
-    expect(response).toMatchObject({
+    const store = await createStore();
+    const modelAPI = new ModelAPI({ store });
+
+    const response = await modelAPI.addModel(testModel);
+    expect(response).toMatch(JSON.stringify({
       message: `Model ${testModel.vendor} ${testModel.modelNumber} already exists!`,
       success: false,
-    });
+    }));
   });
 });
