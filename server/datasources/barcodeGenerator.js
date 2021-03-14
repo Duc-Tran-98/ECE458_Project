3ed2b896fd91110e95503835b24d4269364f1329
Louis Jensen <lbj7@duke.edu>
2021-03-14 19:20:47 -0400

    barcode layout on avery label paper

--- server/datasources/barcodeGenerator.js
+++ server/datasources/barcodeGenerator.js
@@ -1,36 +1,64 @@
+/* eslint-disable no-await-in-loop */
 const JsBarcode = require('jsbarcode');
 const { createCanvas } = require('canvas');
 const fs = require('fs');
 const { PDFDocument } = require('pdf-lib');
 
 const run = async () => {
-  const pdfDoc = await PDFDocument.load(fs.readFileSync('./LabelTemplate.pdf'));
-
   const canvas = createCanvas();
-  // const data = {};
-  const assetTag = 458777;
-  const options = {
-    format: 'CODE128C',
-    text: `HPT Asset    ${assetTag}`,
-    width: 3,
-    height: 30,
-    fontSize: 12,
-    textMargin: 0,
-    margin: 0,
-  };
-  JsBarcode(canvas, assetTag, options);
-  const img = await pdfDoc.embedPng(canvas.toDataURL());
-  // const imagePage = pdfDoc.insertPage(0);
-  pdfDoc.getPage(0).drawImage(img, {
-    x: 30,
-    y: 75,
-    width: 105,
-    height: 30,
-  });
 
   // PAGE VALUES
+  // TOP LEFT BOX 30 723
   // LABEL SIZE HEIGHT 30 WIDTH 105
-  // ROW 1 X 30
+  // TO NEXT BOX SIZE HEIGHT 36 WIDTH 148
+  const data = [];
+  const length = 310; // user defined length
+
+  for (let i = 0; i < length; i += 1) {
+    data.push(1);
+  }
+
+  const pdfDoc = await PDFDocument.create();
+
+  const blank = await PDFDocument.load(fs.readFileSync('./LabelTemplate.pdf'));
+  let blankPage = await pdfDoc.copyPages(blank, blank.getPageIndices());
+  blankPage.forEach((page) => pdfDoc.addPage(page));
+
+  for (let i = 0; i < data.length; i += 1) {
+    const pageNum = Math.floor(i / 80);
+    const xSpot = Math.floor(i / 20) % 4;
+    const ySpot = i % 20;
+    const xVal = 30 + (148 * xSpot);
+    const yVal = 723 - (36 * ySpot);
+
+    const assetTag = 100000 + i;
+    const options = {
+      format: 'CODE128C',
+      text: `HPT Asset    ${assetTag}`,
+      width: 3,
+      height: 30,
+      fontSize: 12,
+      textMargin: 0,
+      margin: 0,
+    };
+    JsBarcode(canvas, assetTag, options);
+    const img = await pdfDoc.embedPng(canvas.toDataURL());
+
+    // console.log(`${pageNum} ${pages.length}`);
+    if (pageNum >= pdfDoc.getPages().length) {
+      blankPage = await pdfDoc.copyPages(blank, blank.getPageIndices());
+      blankPage.forEach((el) => pdfDoc.addPage(el));
+    }
+    const pages = pdfDoc.getPages();
+    // eslint-disable-next-line prefer-const
+    let page = pages[pageNum];
+    page.drawImage(img, {
+      x: xVal,
+      y: yVal,
+      width: 105,
+      height: 30,
+    });
+  }
 
   const pdfBytes = await pdfDoc.save();
   const newFilePath = 'output.pdf';
