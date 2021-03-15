fc24edd6e3ea40aa3586f930cdcda7ea1e84faca
Louis Jensen <lbj7@duke.edu>
2021-03-15 17:12:01 -0400

    generate barcodes working

--- client/src/components/GenerateBarcodes.jsx
+++ client/src/components/GenerateBarcodes.jsx
@@ -3,15 +3,19 @@ import { Button } from 'react-bootstrap';
 import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import ExpressQuery from '../queries/ExpressQuery';
+import { GetAssetTags } from '../queries/GetInstrumentsForExport';
 
 const download = require('downloadjs');
 
 const fs = require('fs');
 
-const GenerateBarcodes = ({ filterOptions }) => {
+const GenerateBarcodes = ({ filterOptions, assetTags, getAll }) => {
   GenerateBarcodes.propTypes = {
     // eslint-disable-next-line react/forbid-prop-types
+    assetTags: PropTypes.array.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
     filterOptions: PropTypes.object.isRequired,
+    getAll: PropTypes.bool.isRequired,
   };
 
   const handleResponse = (response) => {
@@ -20,9 +24,25 @@ const GenerateBarcodes = ({ filterOptions }) => {
     download(blob, 'asset_labels.pdf', 'application/pdf');
   };
 
-  const barcodeQuery = () => {
+  const barcodeQuery = async () => {
+    // ExpressQuery({
+    //   endpoint: '/api/barcodes?tags[]=222222&tags[]=124600&tags[]=121199', method: 'get', queryJSON: { }, handleResponse, responseType: 'arraybuffer',
+    // });
+    if (!getAll && assetTags.length < 1) return;
+    let tagsToGenerate;
+    let expressParam = '/api/barcodes?';
+    if (getAll) {
+      await GetAssetTags({ filterOptions }).then((res) => {
+        tagsToGenerate = res.instruments.map((a) => a.assetTag);
+      });
+    } else {
+      tagsToGenerate = assetTags;
+    }
+    for (let i = 0; i < tagsToGenerate.length; i += 1) {
+      expressParam = expressParam.concat(`${i === 0 ? '' : '&'}tags[]=${tagsToGenerate[i]}`);
+    }
     ExpressQuery({
-      endpoint: '/api/barcodes?tags[]=222222&tags[]=124600&tags[]=121199', method: 'get', queryJSON: { }, handleResponse, responseType: 'arraybuffer',
+      endpoint: expressParam, method: 'get', queryJSON: { }, handleResponse, responseType: 'arraybuffer',
     });
   };
 
--- client/src/queries/GetInstrumentsForExport.jsx
+++ client/src/queries/GetInstrumentsForExport.jsx
@@ -75,3 +75,53 @@ export default async function GetInstrumentsForExport({ filterOptions }) {
   const response = await QueryAndThen({ query, queryName, getVariables });
   return response;
 }
+
+export async function GetAssetTags({ filterOptions }) {
+  GetInstrumentsForExport.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    filterOptions: PropTypes.object.isRequired,
+  };
+  const GET_INSTRUMENTS_QUERY = gql`
+  query Instruments(
+    $vendor: String
+    $modelNumber: String
+    $description: String
+    $serialNumber: String
+    $assetTag: Int
+    $modelCategories: [String]
+    $instrumentCategories: [String]
+  ) {
+    getInstrumentsWithFilter(
+      vendor: $vendor
+      modelNumber: $modelNumber
+      description: $description
+      serialNumber: $serialNumber
+      assetTag: $assetTag
+      modelCategories: $modelCategories
+      instrumentCategories: $instrumentCategories
+    ) {
+      instruments {
+        assetTag
+      }
+    }
+  }
+  `;
+  const query = print(GET_INSTRUMENTS_QUERY);
+  const queryName = 'getInstrumentsWithFilter';
+
+  const serialNumber = typeof filterOptions.filterSerialNumber === 'undefined' ? null : filterOptions.filterSerialNumber;
+  const vendor = typeof filterOptions.vendors === 'undefined' ? null : filterOptions.vendors;
+  const modelNumber = typeof filterOptions.modelNumbers === 'undefined' ? null : filterOptions.modelNumbers;
+  const description = typeof filterOptions.descriptions === 'undefined' ? null : filterOptions.descriptions;
+  const assetTag = (filterOptions.assetTag === null || typeof filterOptions.assetTag === 'undefined') ? null : filterOptions.assetTag;
+  // eslint-disable-next-line prefer-destructuring
+  const modelCategories = filterOptions.modelCategories;
+  // eslint-disable-next-line prefer-destructuring
+  const instrumentCategories = filterOptions.instrumentCategories;
+
+  const getVariables = () => ({
+    vendor, modelNumber, description, serialNumber, assetTag, modelCategories, instrumentCategories,
+  });
+  const response = await QueryAndThen({ query, queryName, getVariables });
+  return response;
+}
