0f9d8531e7175760d90966eb1a9fafc6bfcc92c3
Max Smith <ms724@duke.edu>
2021-03-17 13:20:27 -0400

    Created backup folder with culling scripts

---
+++ backup/culling.py
@@ -0,0 +1,30 @@
+# Culling script to perform staggered retention on database backups
+# Created by Max Smith on 20210317
+
+from os import listdir, name
+from os.path import isfile, join
+
+# File paths on both systems (note to other developers, change windows path)
+PATH_TO_PROJECT="C:\\Users\\smith\\Desktop\\ECE458\\ECE458_Project"
+WINDOWS_PATH=f"{PATH_TO_PROJECT}\\backup\\test"
+LINUX_PATH="/home/vcm/db_backup"
+
+def getOperatingSystem():
+  return name
+
+def getPath():
+  operatingSystem = getOperatingSystem()
+  print(f"operatingSystem: {operatingSystem}")
+  return WINDOWS_PATH if operatingSystem == "nt" else LINUX_PATH
+
+def getFilesInPath(path):
+  return [f for f in listdir(path) if isfile(join(path, f))]
+
+def main():
+  filePath = getPath()
+  allFiles = getFilesInPath(path=filePath)
+  print(allFiles)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
