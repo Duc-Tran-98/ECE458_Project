c0d76a5e2f80113162758f1c3debed898c96c831
Max Smith <ms724@duke.edu>
2021-03-17 14:30:58 -0400

    Updating culling procedure

--- backup/culling.py
+++ backup/culling.py
@@ -1,26 +1,30 @@
 # Culling script to perform staggered retention on database backups
 # Created by Max Smith on 20210317
 
-from os import listdir, name
-from os.path import isfile, join
+# TODO: Ask Bletsch about implementation on 7.2
+# On day 8, 7th daily "falls off" and becomes first weekly
+# On day 31, 4th weekly "falls off" and becomes first monthly
+# On day 366, 12th monthly "falls off" and is removed
+
+from os import name
 import glob
 
-# File paths on both systems (note to other developers, change windows path)
-PATH_TO_PROJECT="C:\\Users\\smith\\Desktop\\ECE458\\ECE458_Project"
-WINDOWS_PATH=f"{PATH_TO_PROJECT}\\backup\\test"
-LINUX_PATH="/home/vcm/db_backup"
+# Static strings for operating
+PATH_TO_PROJECT = "C:\\Users\\smith\\Desktop\\ECE458\\ECE458_Project"
+WINDOWS_PATH = f"{PATH_TO_PROJECT}\\backup\\test"
+LINUX_PATH = "/home/vcm/db_backup"
+WINDOWS_NAME = "nt"
 
 def getOperatingSystem():
   return name
 
 def getPath():
   operatingSystem = getOperatingSystem()
-  print(f"operatingSystem: {operatingSystem}")
-  return WINDOWS_PATH if operatingSystem == "nt" else LINUX_PATH
+  print(f"operatingSystem: {operatingSystem}") # "nt" on Windows, "posix" on vcm
+  return WINDOWS_PATH if operatingSystem == WINDOWS_NAME else LINUX_PATH
 
 def getFilesInPath(path):
   return glob.glob(f"{path}/backup_*.zip")
-  # return [f for f in listdir(path) if isfile(join(path, f))]
 
 def main():
   filePath = getPath()
