21fe967c80b537dc32075dcf40fa4a122bec7a24
Max Smith <ms724@duke.edu>
2021-04-01 16:59:45 -0400

    refactored form builder with less state

--- client/src/components/CustomFormBuilder.jsx
+++ client/src/components/CustomFormBuilder.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import CustomFormStep from './CustomFormStep';
 import LinkedList from '../objects/LinkedList';
 
-// TODO: Lift state from form step into parent
-// TODO: Arbitrary ordering, create, delete of steps
-// TODO: Linked list for position (when you add something from middle, insert)
+// TODO: Keep state synced with child component
 export default function CustomFormBuilder() {
   const emptyState = {
     header: '',
@@ -17,45 +15,34 @@ export default function CustomFormBuilder() {
     textLabel: '',
   };
   let nextId = 1;
-  const initState = new Map().set(0, emptyState); // TODO: Make shallow copy of state
-  const [state, setState] = React.useState(initState);
+  const [update, setUpdate] = React.useState(0);
   const [formSteps, setFormSteps] = React.useState([]);
   const [size, setSize] = React.useState(1);
   const linkedList = new LinkedList(0);
+  const map = new Map().set(0, Object.create(emptyState));
 
   const handleSubmit = () => {
-    console.log('submitting form with state: ');
-    console.log(state);
+    console.log('submitting form with map: ');
+    console.log(map);
   };
   const updateState = (id, event, value) => {
     console.log(`updateState(${id}, ${event}, ${value})`);
-    const nextState = state;
-    nextState.set(id, {
-      ...state.get(id),
+    map.set(id, {
+      ...map.get(id),
       [event]: value,
     });
-    setState(nextState);
+    setUpdate(update + 1);
   };
   const createStep = (id) => {
     console.log(`createStep after id: ${id}`);
-    const nextState = state;
-    nextState.set(nextId, emptyState);
-    setState(nextState);
+    map.set(id, Object.create(emptyState));
     linkedList.insertAfter(id, nextId);
-    console.log('nextState and then linkedList');
-    console.log(nextState);
-    console.log(linkedList);
     nextId += 1;
-    setSize(linkedList.size());
   };
   const deleteStep = (id) => {
-    console.log(`deleteStep from id: ${id}`);
-    const nextState = state;
-    nextState.delete(id);
-    setState(nextState);
+    console.log(`deleteStep with id: ${id}`);
+    map.delete(id);
     linkedList.deleteNode(id);
-    console.log(linkedList);
-    setSize(linkedList.size());
   };
 
   const generateKeysArray = () => {
@@ -75,7 +62,7 @@ export default function CustomFormBuilder() {
       <CustomFormStep
         key={key}
         id={key}
-        state={state.get(key)}
+        state={map.get(key)}
         updateState={updateState}
         createStep={createStep}
         deleteStep={deleteStep}
@@ -84,7 +71,8 @@ export default function CustomFormBuilder() {
     console.log('formSteps: ');
     console.log(steps);
     setFormSteps(steps);
-  }, state);
+    setSize(linkedList.size());
+  }, update);
 
   return (
     <>
