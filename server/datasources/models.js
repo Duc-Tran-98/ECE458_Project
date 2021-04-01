883b1c5e7b71e5314b5374f217a858bdc87893db
Louis Jensen <lbj7@duke.edu>
2021-04-01 00:40:14 -0400

    validate one calibration mode server side;

--- server/datasources/models.js
+++ server/datasources/models.js
@@ -4,7 +4,7 @@ const { DataSource } = require('apollo-datasource');
 const SQL = require('sequelize');
 
 function validateModel({
-  modelNumber = '', vendor = '', description = '', comment = '', klufe = false, loadBank = false,
+  modelNumber = '', vendor = '', description = '', comment = '', klufe = false, loadBank = false, custom = false,
 }) {
   if (vendor.length > 30) {
     return [false, 'ERROR: Vendor input must be under 30 characters!'];
@@ -27,8 +27,12 @@ function validateModel({
   if (comment != null && comment.length > 2000) {
     return [false, 'ERROR: Comment input must be under 2000 characters!'];
   }
-  if (klufe && loadBank) {
-    return [false, 'ERROR: Model cannot support calibration via load bank wizard and Klufe 5700!'];
+  let count = 0;
+  if (klufe) count += 1;
+  if (loadBank) count += 1;
+  if (custom) count += 1;
+  if (count > 1) {
+    return [false, 'ERROR: Model can only support one of load bank wizard, klufe wizard, or custom form!'];
   }
   return [true];
 }
@@ -115,7 +119,7 @@ class ModelAPI extends DataSource {
     }
     const validation = validateModel({
       // eslint-disable-next-line max-len
-      modelNumber, vendor, description, comment, klufe: supportKlufeCalibration, loadBank: supportLoadBankCalibration,
+      modelNumber, vendor, description, comment, klufe: supportKlufeCalibration, loadBank: supportLoadBankCalibration, custom: supportCustomCalibration,
     });
     if (!validation[0]) {
       // eslint-disable-next-line prefer-destructuring
@@ -424,7 +428,7 @@ class ModelAPI extends DataSource {
     }
     const validation = validateModel({
       // eslint-disable-next-line max-len
-      modelNumber, vendor, description, comment, klufe: supportKlufeCalibration, loadBank: supportLoadBankCalibration,
+      modelNumber, vendor, description, comment, klufe: supportKlufeCalibration, loadBank: supportLoadBankCalibration, custom: supportCustomCalibration,
     });
     if (!validation[0]) {
       // eslint-disable-next-line prefer-destructuring
