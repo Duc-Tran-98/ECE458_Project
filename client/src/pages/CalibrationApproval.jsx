b0ce4265d936992e3f2ad9ad80837b2606813ad4
Angel Huizar <ah426@duke.edu>
2021-04-06 15:40:06 -0700

    got initial page up and running

--- client/src/pages/CalibrationApproval.jsx
+++ client/src/pages/CalibrationApproval.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { ServerPaginationGrid } from '../components/UITable';
 import UserContext from '../components/UserContext';
+import { cols } from '../utils/CalibTable';
+import GetCalibHistory from '../queries/GetCalibHistory';
 
 export default function CalibrationApprovalPage() {
+  // eslint-disable-next-line no-unused-vars
   const user = React.useContext(UserContext);
   const history = useHistory();
   const queryString = window.location.search;
@@ -12,6 +15,7 @@ export default function CalibrationApprovalPage() {
   const [initLimit, setInitLimit] = React.useState(parseInt(urlParams.get('limit'), 10));
   const [orderBy, setOrderBy] = React.useState(urlParams.get('orderBy'));
   const [sortBy, setSortBy] = React.useState(urlParams.get('sortBy'));
+  // eslint-disable-next-line no-unused-vars
   const [update, setUpdate] = React.useState(false);
   const getAndSetUrlVals = (search = null) => {
     const urlVals = new URLSearchParams(search || window.location.search);
@@ -23,6 +27,25 @@ export default function CalibrationApprovalPage() {
     setInitPage(pg);
     setOrderBy(order);
     setSortBy(sort);
+  };
+  const updateUrlOnPageChange = (page, limit) => {
+    // this is passed to the on page change and on page size change
+    // handlers of the server pagination grid
+    const searchString = `?page=${page}&limit=${limit}&orderBy=${orderBy}&sortBy=${sortBy}`;
+    if (window.location.search !== searchString) {
+      // If current location != next location, update url
+      history.push(`/viewModels${searchString}`);
+    }
+  };
+
+  const updateUrlOnOrderChange = (order, sort) => {
+    // this is passed to the on page change and on page size change
+    // handlers of the server pagination grid
+    const searchString = `?page=${initPage}&limit=${initLimit}&orderBy=${order}&sortBy=${sort}`;
+    if (window.location.search !== searchString) {
+      // If current location != next location, update url
+      history.push(`/viewModels${searchString}`);
+    }
   };
   React.useEffect(() => {
     history.listen((location) => {
@@ -38,6 +61,31 @@ export default function CalibrationApprovalPage() {
     });
   }, []);
   return (
-    <ServerPaginationGrid />
+    <ServerPaginationGrid
+      shouldUpdate={update}
+      cols={cols}
+      initPage={initPage || 1}
+      initLimit={initLimit || 25}
+      onPageChange={(page, limit) => {
+        updateUrlOnPageChange(page, limit);
+      }}
+      onPageSizeChange={(page, limit) => {
+        updateUrlOnPageChange(page, limit);
+      }}
+      initialOrder={orderBy ? [[orderBy, sortBy]] : []}
+      onSortModelChange={(order, sort) => {
+        updateUrlOnOrderChange(order, sort);
+      }}
+      showToolBar={false}
+      showImport={false}
+      headerElement={(
+        <div className="ps-3 h5 py-2">
+          Calibration Approval Table
+        </div>
+      )}
+      // eslint-disable-next-line no-unused-vars
+      fetchData={(limit, offset, ordering) => GetCalibHistory({ id: 1 }).then((data) => data)}
+      rowCount={() => GetCalibHistory({ id: 1 }).then((data) => data.length)}
+    />
   );
 }
