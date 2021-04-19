6cdc982b2f4809411dcc8190a90dece35dd7b2ee
Angel Huizar <ah426@duke.edu>
2021-04-19 16:47:05 -0700

    working on production ws

--- server/index.js
+++ server/index.js
@@ -106,7 +106,11 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(({ url, subscriptionsUrl }) => {
+const wsRoute = process.env.NODE_ENV.includes('dev')
+  ? 'ws://localhost:4000/graphql'
+  : 'wss://localhost:4000/graphql';
+
+server.listen({ subscriptionsUrl: wsRoute }).then(({ url, subscriptionsUrl }) => {
   console.log(`
     Server is running at ${url}\n
     Subscriptions Ready at ${subscriptionsUrl}
